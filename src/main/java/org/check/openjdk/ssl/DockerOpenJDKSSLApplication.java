34b37112932a130ff88f43d36a76fcdd36953304
Maurizio <mauriziocasciano7@gmail.com>
2021-10-14 10:48:44 +0200

    Spring init

---
+++ src/main/java/org/check/openjdk/ssl/DockerOpenJDKSSLApplication.java
@@ -0,0 +1,13 @@
+package org.check.openjdk.ssl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerOpenJDKSSLApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerOpenJDKSSLApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/check/openjdk/ssl/DockerOpenJDKSSLApplicationTests.java
@@ -0,0 +1,13 @@
+package org.check.openjdk.ssl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerOpenJDKSSLApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
